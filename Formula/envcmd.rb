664b9a4b8224c3b085887502c25354f74569508c
nullbrna <252729190+nullbrna@users.noreply.github.com>
2026-02-20 21:16:27 +0000

    bump(envcmd): v1.1.0

--- Formula/envcmd.rb
+++ Formula/envcmd.rb
@@ -1,8 +1,8 @@
 class Envcmd < Formula
     desc "Environment-specific and configurable commands"
     homepage "https://github.com/nullbrna/envcmd"
-    url "https://github.com/nullbrna/envcmd/releases/download/v1.0.0/envcmd_v1.0.0_darwin_arm64.tar.gz"
-    sha256 "c87ad966d00d5079cfa1539073ecb4b8004df9a771dbb0a8c1efce614092868a"
+    url "https://github.com/nullbrna/envcmd/releases/download/v1.1.0/envcmd_v1.1.0_darwin_arm64.tar.gz"
+    sha256 "adec17cce05444bd3c08fe2818ac0580c9b50026250b48d44d10aea9a62ebb3b"
     license "MIT"
 
     depends_on arch: :arm64
